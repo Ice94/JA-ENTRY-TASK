2130da991055486b2c3b2413097ff6cbe5c23ade
mhbratek <mhbratek@gmail.com>
2017-05-15 10:57:08 +0200

    Add board and size filed.

--- src/main/java/epam/tictactoe/TicTacToe.java
+++ src/main/java/epam/tictactoe/TicTacToe.java
@@ -4,5 +4,13 @@ package epam.tictactoe;
  * Created by Mateusz on 2017-05-15.
  */
 public class TicTacToe {
+    char board[][];
+    int size;
+
+    public TicTacToe(int size) {
+        this.size = size;
+        board = new char[size][size];
+    }
+
 
 }
